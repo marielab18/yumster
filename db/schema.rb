8872a659509278e2bef825a4136849e2158b5be4
Etienne Eisfeller <eeisfeller@gmail.com>
2022-03-02 12:20:12 +0100

    added coloumn instructions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_163346) do
+ActiveRecord::Schema.define(version: 2022_03_02_111805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_02_28_163346) do
     t.bigint "recipe_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "comment"
     t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
@@ -48,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_163346) do
     t.text "nutritional_info"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "instructions"
   end
 
   create_table "users", force: :cascade do |t|
