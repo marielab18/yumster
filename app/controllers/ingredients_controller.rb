745829766f35e1263ba363942dc3cde341a54fed
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-03-02 15:51:48 +0100

    Add the home method to show the categories of the ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,5 @@
 class IngredientsController < ApplicationController
-  def index
-  end
-
   def show
+    @ingredient = Ingredient.find(:id)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @ingredients_carbs = Ingredient.where(category: "Carbs")
+    @ingredients_protein = Ingredient.where(category: "Protein")
+    @ingredients_vegetable = Ingredient.where(category: "Vegetable")
   end
 end
