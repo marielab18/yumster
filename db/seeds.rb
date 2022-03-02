82903d8bf58f42fbe1f2b2e7e63e025855b979b5
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-02 16:21:45 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lorem = '
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum id saepe eligendi ducimus. Corrupti, perspiciatis quo ex distinctio ipsam, dolorum nihil porro, placeat recusandae ad accusantium illo. Sint, soluta?
+'
+
+puts 'Create Users'
+
+User.create!(
+  email: 'player@one.com',
+  password: 123456
+)
+
+puts 'Create Ingredients'
+
+Ingredient.create!(
+  name: 'Chicken',
+  category: 'Protein'
+)
+
+Ingredient.create!(
+  name: 'Noodles',
+  category: 'Carb'
+)
+
+puts 'Create Recipes'
+
+Recipe.create!(
+  name: 'Curly 🐔 Chicken',
+  description: 'Not chicken nuggets, but great',
+  minutes: 5,
+  nutritional_info: lorem,
+  instructions: lorem
+)
+
+puts 'Connect Recipe to Ingredients'
+
+# you can use this syntax
+IngredientRecipe.create!(
+  recipe_id: Recipe.first.id,
+  ingredient_id: Ingredient.first.id
+)
+
+# or this syntax
+Recipe.first.ingredients << Ingredient.last
+
+puts 'Create Favorites'
+
+Favorite.create!(
+  user: User.first,
+  recipe: Recipe.first
+)
