e61bd18cd294d57312adbbd1ea2ec5f000226c15
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-03-03 12:59:19 +0100

    Show and half of the selection controller done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,29 @@
 class RecipesController < ApplicationController
   def selection
+    @recipe = Recipe.find(params[:id])
+
+
+    #search with the ingredirents
+    #@recipes = Recipe.select(params)
   end
 
+  # yes to the show page with all the instructions
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
+  # no -take me to the next recipe
   def update
+    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.update(recipe_params)
+
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:id, :rating, :address)
   end
 
 end
