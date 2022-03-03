8e7f418b34dad0ac0300e0e7010a9d39c3cc668a
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-03-03 17:59:47 +0100

    Adding the bookmarks/favorites

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,7 @@
 class RecipesController < ApplicationController
   def selection
-    if Recipe.find(params[:id]).present?
-
-      @recipe = Recipe.find(params[:id])
-    else
-
-      @recipe = Recipe.first
-
-    end
-
-
-
+    @recipe = Recipe.find(params[:id])
+    @favorite = current_user.favorites.find_by(recipe: @recipe)
     #search with the ingredirents
     #@recipes = Recipe.select(params)
   end
@@ -18,20 +9,9 @@ class RecipesController < ApplicationController
   # yes to the show page with all the instructions
   def show
     @recipe = Recipe.find(params[:id])
+    @favorite = current_user.favorites.find_by(recipe: @recipe)
   end
 
-  # no -take me to the next recipe
-  def update
-    @recipe = Recipe.find(params[:id])
-    @recipe = Recipe.update(recipe_params)
 
-    redirect_to recipe_path(@recipe)
-  end
-
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:id, :rating, :address)
-  end
 
 end
