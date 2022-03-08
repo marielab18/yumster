72d3cac6b13a17b05a0b987a8f74722c194beebe
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-08 11:42:23 +0100

    use scraper in the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 require "open-uri"
+require_relative 'scraper'
+
 
 lorem = '
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum id saepe eligendi ducimus. Corrupti, perspiciatis quo ex distinctio ipsam, dolorum nihil porro, placeat recusandae ad accusantium illo. Sint, soluta?
@@ -6,9 +8,10 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum id saepe el
 
 def build(items, model_class)
   items.each do |item|
-    url = item.delete(:url)
+    puts "Building #{item[:name]} #{model_class}"
+    image_url = item.delete(:image_url)
     model = model_class.create!(item)
-    file = URI.open(url)
+    file = URI.open(image_url)
     model.photo.attach(io: file, filename: "#{item[:name].downcase}.png", content_type: 'image/png')
   end
 end
@@ -29,16 +32,16 @@ User.create!(
 puts 'Create Ingredients'
 
 ingredients = [
-  { name: 'Noodles', category: 'Carb', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646318741/development/massa_tpwggn.png' },
-  { name: 'Rice', category: 'Carb', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646319962/development/rice_erdar0.png' },
-  { name: 'Chicken', category: 'Protein', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646317382/chicken_mmehqa.png' },
-  { name: 'Shrimp', category: 'Protein', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320139/development/shrimp_yh4iir.png' },
-  { name: 'Beef', category: 'Protein', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320828/development/vaquinha_wvsu1s.png' },
-  { name: 'Salmon', category: 'Protein', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321716/development/salmon_ocnmrr.png' },
-  { name: 'Carrot', category: 'Vegetable', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320914/development/carrot_iacktx.png' },
-  { name: 'Brocolli', category: 'Vegetable', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321046/development/broccoli_sumyza.png' },
-  { name: 'Onion', category: 'Vegetable', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321329/development/onion_nsck5e.png' },
-  { name: 'Garlic', category: 'Vegetable', url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321523/development/garlic_dldlss.png' },
+  { name: 'Noodles', category: 'Carb', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646318741/development/massa_tpwggn.png' },
+  { name: 'Rice', category: 'Carb', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646319962/development/rice_erdar0.png' },
+  { name: 'Chicken', category: 'Protein', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646317382/chicken_mmehqa.png' },
+  { name: 'Shrimp', category: 'Protein', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320139/development/shrimp_yh4iir.png' },
+  { name: 'Beef', category: 'Protein', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320828/development/vaquinha_wvsu1s.png' },
+  { name: 'Salmon', category: 'Protein', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321716/development/salmon_ocnmrr.png' },
+  { name: 'Carrot', category: 'Vegetable', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646320914/development/carrot_iacktx.png' },
+  { name: 'Brocolli', category: 'Vegetable', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321046/development/broccoli_sumyza.png' },
+  { name: 'Onion', category: 'Vegetable', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321329/development/onion_nsck5e.png' },
+  { name: 'Garlic', category: 'Vegetable', image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646321523/development/garlic_dldlss.png' },
 ]
 
 build(ingredients, Ingredient)
@@ -52,7 +55,7 @@ recipes = [
     minutes: '10Min',
     nutritional_info: '500 Kcal',
     instructions: lorem,
-    url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322010/development/curlychicken_ubqfxe.png'
+    image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322010/development/curlychicken_ubqfxe.png'
   },
   {
     name: 'Spicy Chicken',
@@ -60,7 +63,7 @@ recipes = [
     minutes: '15Min',
     nutritional_info: '500 Kcal',
     instructions: lorem,
-    url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322242/development/spicychicken_gwlhdw.jpg'
+    image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322242/development/spicychicken_gwlhdw.jpg'
   },
   {
     name: 'Shrimp Pasta',
@@ -68,7 +71,7 @@ recipes = [
     minutes: '10Min',
     nutritional_info: '500 Kcal',
     instructions: lorem,
-    url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322458/development/garlicshrimppasta_qiltqd.jpg'
+    image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322458/development/garlicshrimppasta_qiltqd.jpg'
   },
   {
     name: 'Ceasar Salad',
@@ -76,7 +79,7 @@ recipes = [
     minutes: '10Min',
     nutritional_info: '300 Kcal',
     instructions: lorem,
-    url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322819/development/caesarsalad_lruxdq.jpg'
+    image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322819/development/caesarsalad_lruxdq.jpg'
   },
   {
     name: 'Beef Soup',
@@ -84,7 +87,7 @@ recipes = [
     minutes: '20Min',
     nutritional_info: '500 Kcal',
     instructions: lorem,
-    url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322982/development/beefsoup_bp5rn8.jpg'
+    image_url: 'https://res.cloudinary.com/dv5lwgtlx/image/upload/v1646322982/development/beefsoup_bp5rn8.jpg'
   }
 ]
 
@@ -136,3 +139,10 @@ Favorite.create!(
   user: User.first,
   recipe: Recipe.first
 )
+
+puts 'Start scraping'
+
+ingredient = 'chicken'
+puts 'Scraping recipes with ' + ingredient.capitalize
+chicken_recipes = scrape(ingredient)
+build(chicken_recipes, Recipe)
